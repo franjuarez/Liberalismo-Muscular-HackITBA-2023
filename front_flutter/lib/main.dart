f1717110bab38b63a3c1495e5bbbb507be9f9264
francisco juarez <Francisco Juarez>
2023-04-01 12:06:55 -0300

    conectado back con front ppara los dos lados

--- front_flutter/lib/main.dart
+++ front_flutter/lib/main.dart
@@ -3,25 +3,37 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
-}
-
-Future<void> addPassword(String user, String domain, String username, String password) async {
-  var url = Uri.parse('http://10.7.17.82:8000/$user/add_password');
-  var response = await http.post(url, body: {
-    'user': user,'domain': domain,'username': username, 'password': password});
-
-  print('Response status: ${response.statusCode}');
-  print('Response body: ${response.body}');
-}
-
 String user = 'Monke';
 String domain = 'Microsoft';
 String username = 'lemonke';
 String password = 'tetas12';
 
-// addPassword(user, domain, username, password);
+String domain1 = 'Gugul';
+String username1 = 'monkesin';
+String password1 = 'tetas13';
+
+String user2 = 'Valen';
+String domain2 = 'Gugul';
+String username2 = 'monkesin';
+String password2 = 'tetas13';
+
+void main() {
+  addPassword(user, domain, username, password);
+  addPassword(user, domain1, username1, password1);
+  addPassword(user2, domain2, username2, password2);
+  runApp(const MyApp());
+}
+
+Future<void> addPassword(
+    String user, String domain, String username, String password) async {
+  var url = Uri.parse(
+      'http://10.7.17.82:8000/add_password?user=$user&domain=$domain&username=$username&password=$password');
+  var response = await http.post(url);
+
+  //To check if the response is OK
+  // print('Response status: ${response.statusCode}');
+  // print('Response body: ${response.body}');
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -42,16 +54,16 @@ class MyApp extends StatelessWidget {
               //de acá para abajo hago boludeces con esa data
 
               return ListView.builder(
-                itemCount: data.length,
+                itemCount: 1,
                 itemBuilder: (context, index) {
                   return ListTile(
-                    title: Text((data[index]).toString()),
-                    // title: Text(data[index]['name']),
+                    title: Text((data).toString()),
                   );
                 },
               );
             } else if (snapshot.hasError) {
               return Text("${snapshot.error}");
+
             }
             return const CircularProgressIndicator();
           },
